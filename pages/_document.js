b246ee9a41963aacf17bcc802e3d79d20344b11f
Sarah and Jon <cox.jonathan@gmail.com>
2022-07-03 18:13:38 -0400

    initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,15 +3,15 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
+    console.log(initialProps)
     return { ...initialProps }
   }
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
-      image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+      title: 'Jonathan Cox - Software Engineer - Web Developer',
+      description:
+        'Blog and Brain Dump of ideas in Software Engineering, User Experience, User Interface, and Web Development'
     }
 
     return (
@@ -23,11 +23,6 @@ class MyDocument extends Document {
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
           <meta property="og:image" content={meta.image} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@yourname" />
-          <meta name="twitter:title" content={meta.title} />
-          <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
         </Head>
         <body>
           <Main />
---
+++ pages/posts/fail_fast.md
@@ -0,0 +1,22 @@
+---
+title: Fail Fast and Delete it All
+date: 2022/02/22
+description: Know when to quite and restart
+tag: learning, progress
+---
+
+02/22/2022
+
+> Everything's broken and nothing works
+
+Everything's broken and nothing works, but why? You've taken the time to debug, maybe you've called upon a co-worker. After some time, you're still in the same spot, spinning, going nowhere.
+
+> You're in the same spot, spinning, going nowhere.
+
+I now know, after being in the situation I've outline above, that the answer is to stop, take a break, and **delete** it all. Now I say delete in the metaphorical sense, don't go deleting the entire solution or project. Get yourself back to square one, read over the req's and think. Don't do anything besides **think**.
+
+You've now taken time to absorb the problem and you're ready to move forward. You find yourself approaching the solution with a new set of eyes, dodging the traps you unknowingly laid for yourself in the previous iteration. This sort of insight is invaluable, and in my opinion, cannot be taught. You only achieve this sort of 'awakening' through **failure**, so don't be afraid, we all fail.
+
+> Make failing a habit, failing proves that you are pushing yourself and the concepts and ideas you have.
+
+Call it quits, **delete** it all and just **think**.
